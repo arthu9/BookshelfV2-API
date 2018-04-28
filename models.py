8dd662a9ac13bbbf392d0cdb29868a10961348a5
mikeeevillanueva <mikeeevillanueva@gmail.com>
2018-04-28 16:07:57 +0800

    update database url

--- models.py
+++ models.py
@@ -3,6 +3,15 @@ from flask import Flask
 import sqlalchemy, datetime
 from flask_login import UserMixin
 from app import app
+import os
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mvjunetwo@127.0.0.1:5432/bookshelf'
+# engine = sqlalchemy.create_engine('postgresql://postgres:pagararea1096@127.0.0.1:5432/bookshelf')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'thisisthesecretkey'
 
 db = SQLAlchemy(app)
 
