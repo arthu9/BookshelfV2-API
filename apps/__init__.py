123eeefb52e909f5b311d7543996ebdf7f6fbc26
mikeeevillanueva <mikeeevillanueva@gmail.com>
2018-05-04 19:14:28 +0800

    heroku deploy

--- apps/__init__.py
+++ apps/__init__.py
@@ -11,28 +11,31 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
+db = SQLAlchemy(app)
+
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mvjunetwo@localhost/bookshelf'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mvjunetwo@localhost/bookshelf'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['USE_SESSION_FOR_NEXT'] = True
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['SECRET_KEY'] = 'thisissecret'
 app.secret_key = os.urandom(24)
 
-db = SQLAlchemy(app)
+#rttr
 
-import apps.api
+from apps import api
 
 
-def createDB():
-    engine = sqlalchemy.create_engine('postgresql://postgres:mvjunetwo@localhost') #connects to server
-    conn = engine.connect()
-    conn.execute("commit")
-    conn.execute("create database bookshelf")
-    conn.close()
+#def createDB():
+#    engine = sqlalchemy.create_engine('postgresql://postgres:mvjunetwo@localhost') #connects to server
+#    conn = engine.connect()
+#    conn.execute("commit")
+#    conn.execute("create database bookshelf")
+#    conn.close()A
 
-def createTables():
-    db.create_all()
+#def createTables():
+db.create_all()
 
 #createDB()
-createTables()
+#createTables()
