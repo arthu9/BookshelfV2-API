06d85e7113a1b31c192f30ff3d187d18bb7478a7
weapagawa <reiiyahh25@gmail.com>
2018-05-27 23:39:16 +0800

    [update] follow and unfollow -- added db

--- apps/models.py
+++ apps/models.py
@@ -407,33 +407,6 @@ class FollowersAssociation(db.Model):
 #     numberFollow = db.Column(db.Integer)
 #
 #     def __init__(self, userFollowee='', userFollower='', totalFollower=''):
-#         self.userFollower = userFollower
+#         self.userFollower = userFol   lower
 #         self.userFollowee = userFollowee
 #         self.totalFollower = totalFollower
-
-
-# follow-follow
-
-
-
-
-# followed = db.relationship(
-#     'User', secondary=followers,
-#     primaryjoin=(followers.c.follower_id == id),
-#     secondaryjoin=(followers.c.followed_id == id),
-#     backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
-#
-
-#
- # def followed_posts(self):
- #        return Post.query.join(
- #            followers, (followers.c.followed_id == Post.user_id)).filter(
- #                followers.c.follower_id == self.id).order_by(
- #                    Post.timestamp.desc())
-#
-# def followed_posts(self):
-#     followed = Post.query.join(
-#         followers, (followers.c.followed_id == Post.user_id)).filter(
-#         followers.c.follower_id == self.id)
-#     own = Post.query.filter_by(user_id=self.id)
-#     return followed.union(own).order_by(Post.timestamp.desc())
